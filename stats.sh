2d1776773a4e4345b9f9bfd2f2a00559be3017c1
rakesh <rakeshavulapati1999@gmail.com>
2023-08-19 13:47:46 +0530

    commit 3

---
+++ stats.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+#Printing the current date, time, username, and current working directory
+echo "Hello, $LOGNAME" 
+echo "Current date is 'date'" 
+echo "User is 'who i am'" 
+echo "Current directory 'pwd'"
